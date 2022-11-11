714e86428174d9df0570f945815d869ea7f12c71
scfeeley <scfeeley@wpi.edu>
2022-11-11 13:43:04 -0700

    Adding spinalCaseCode

---
+++ FreeCodeCamp/spinalCase.js
@@ -0,0 +1,11 @@
+function spinalCase(str) {
+    //find all instances of lowercase letter followed by uppercase letter
+    let lowerUpper = /([a-z])([A-Z])/g
+    //find all instances of non-alphanumeric characters or underscores
+    let spaceUnderScore = /([\W]|_+)/g
+    //replace lower-upper instances with lowercase letter 'space' uppercase letter
+    let lowerUpperToSpace = str.replace(lowerUpper, '$1 $2')
+    //replace all non-alphanumeric or underscores with a dash
+    let spinal = lowerUpperToSpace.replace(spaceUnderScore, '-').toLowerCase();
+    return spinal;
+  }
